b06d8e7012a1120a9c3fffbcff945ea97005ecb7
mam7 <mam7@SEA-D211-01L.birmingham.k12.mi.us>
2017-01-10 17:50:02 -0500

    added ints

--- 2017FRCProject/src/org/usfirst/frc/team2960/robot/RobotMap.java
+++ 2017FRCProject/src/org/usfirst/frc/team2960/robot/RobotMap.java
@@ -1,9 +1,9 @@
 package org.usfirst.frc.team2960.robot;
 
 public class RobotMap {
-	public static int rt1;
-	public static int rt2;
-	public static int lt1;
-	public static int lt2;
+	public static int rt1 = 0;
+	public static int rt2 = 1;
+	public static int lt1 = 2;
+	public static int lt2 = 3;
 	
 }
