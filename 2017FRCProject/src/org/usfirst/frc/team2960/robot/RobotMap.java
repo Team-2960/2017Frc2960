a1232a9f5d0471e3d0162562a40fbd42c5e8df9e
mam7 <mam7@SEA-D211-01L.birmingham.k12.mi.us>
2017-01-18 18:51:49 -0500

    Comp Drive

--- 2017FRCProject/src/org/usfirst/frc/team2960/robot/RobotMap.java
+++ 2017FRCProject/src/org/usfirst/frc/team2960/robot/RobotMap.java
@@ -3,7 +3,13 @@ package org.usfirst.frc.team2960.robot;
 public class RobotMap {
 	public static int rt1 = 0;
 	public static int rt2 = 1;
-	public static int lt1 = 2;
-	public static int lt2 = 3;
+	public static int rt3 = 2;
+	public static int lt1 = 3;
+	public static int lt2 = 4;
+	public static int lt3 = 5;
+	public static int shooter = 6;
+	public static int winch = 7;
+	public static int intake = 8;
+	
 	public static int photoeye = 0;
 }
--- 2017FRCProject/src/org/usfirst/frc/team2960/robot/subsystems/DriveTrain.java
+++ 2017FRCProject/src/org/usfirst/frc/team2960/robot/subsystems/DriveTrain.java
@@ -12,23 +12,29 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 public class DriveTrain extends Subsystem implements PeriodicUpdate  {
 	CANTalon rt1;
 	CANTalon rt2;
+	CANTalon rt3;
 	CANTalon lt1;
 	CANTalon lt2;
+	CANTalon lt3;
 	DigitalInput photoeye;
 	
 	public DriveTrain(){
 		rt1 = new CANTalon(RobotMap.rt1);
 		rt2 = new CANTalon(RobotMap.rt2);
+		rt3 = new CANTalon(RobotMap.rt3);
 		lt1 = new CANTalon(RobotMap.lt1);
 		lt2 = new CANTalon(RobotMap.lt2);
+		lt3 = new CANTalon(RobotMap.lt3);
 		photoeye = new DigitalInput(RobotMap.photoeye);
 		
 	}
 	public void setSpeed(double right, double left){
 		rt1.set(right);
 		rt2.set(right);
+		rt3.set(right);
 		lt1.set(-left);
 		lt2.set(-left);
+		lt3.set(-left);
 	}
 	@Override
 	public void update() {
@@ -46,8 +52,10 @@ public class DriveTrain extends Subsystem implements PeriodicUpdate  {
 	protected void initDefaultCommand() {
 		rt1.set(0);
 		rt2.set(0);
+		rt3.set(0);
 		lt1.set(0);
 		lt2.set(0);
+		lt3.set(0);
 		
 	}
 
