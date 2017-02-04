2083243fe824e09c2dcd07721e8b0343e197706f
axelman03 <axelman03@gmail.com>
2017-02-04 11:08:00 -0500

    Made the robot go, go, go!!!!!!

--- 2017FRCProject/src/org/usfirst/frc/team2960/robot/OI.java
+++ 2017FRCProject/src/org/usfirst/frc/team2960/robot/OI.java
@@ -12,16 +12,32 @@ public class OI{
 	
 	public void driveRobot(Joystick stick,  DriveTrain drive, Shooter shoot, Intake intake, Agitator agi, GearPusher push){
 		drive.setSpeed(stick.getRawAxis(5), -stick.getRawAxis(1));
-		if(stick.getRawButton(1))
-			shoot.turnONOFF(true);
-		if(stick.getRawButton(2))
-			shoot.turnONOFF(false);
+		if(stick.getRawButton(6))
+			drive.shift(true);
+		if(stick.getRawButton(5))
+			drive.shift(false);
 	}
 	
 	public void operateRobot(Joystick stick, DriveTrain drivetrain, Shooter shoot, Intake intake, Agitator agi, GearPusher push ){
-		if(stick.getRawButton(0)){
-			
-		}
+		if(stick.getRawButton(2))
+			push.turnOn();
+		if(stick.getRawButton(4))
+			push.turnOff();
+		
+		if(stick.getRawButton(6))
+			agi.startAgitator();
+		if(stick.getRawButton(5))
+			agi.stopAgitator();
+		
+		if(stick.getRawButton(1))
+			shoot.turnONOFF(true);
+		if(stick.getRawButton(3))
+			shoot.turnONOFF(false);
+		
+		if(stick.getRawButton(8))
+			intake.startIntake();
+		if(stick.getRawButton(7))
+			intake.stopIntake();
 	}
 	
 }
