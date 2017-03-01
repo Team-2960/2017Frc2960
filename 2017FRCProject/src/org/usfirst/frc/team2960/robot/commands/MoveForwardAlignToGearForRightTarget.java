70257319ed57918866128873f79ca1452af42080
axelman03 <axelman03@gmail.com>
2017-02-28 19:15:15 -0500

    Created boiler side autonomous for gear

--- 2017FRCProject/src/org/usfirst/frc/team2960/robot/commands/MoveForwardAlignToGearForRightTarget.java
+++ 2017FRCProject/src/org/usfirst/frc/team2960/robot/commands/MoveForwardAlignToGearForRightTarget.java
@@ -4,6 +4,11 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 
 public class MoveForwardAlignToGearForRightTarget extends CommandGroup {
 	public MoveForwardAlignToGearForRightTarget(){
-		
-	}
+		addSequential(new MoveForward(-85));
+		addSequential(new TurnDegree(.2, -40));
+		addSequential(new AlignToGear());
+		addSequential(new Delay(20));
+		addSequential(new MoveForwardOrHitButton(-15));
+		addSequential(new MoveForward(20));
+		}
 }
--- 2017FRCProject/src/org/usfirst/frc/team2960/robot/commands/TurnDegree.java
+++ 2017FRCProject/src/org/usfirst/frc/team2960/robot/commands/TurnDegree.java
@@ -1,17 +1,50 @@
 package org.usfirst.frc.team2960.robot.commands;
 
+import org.usfirst.frc.team2960.robot.Robot;
+
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.command.Command;
 
 public class TurnDegree extends Command{
 
+	boolean done;
+	double seconds;
+	double rate;
+	Timer lengthOfTurn;
 	
+	public TurnDegree(double seconds, double rate){
+		super("TurnDegree");
+		requires(Robot.drivetrain);
+		this.seconds = seconds;
+		this.rate = rate;
+		lengthOfTurn = new Timer();
+		Robot.drivetrain.isTurnOnly = false;
+		done = false;
+	}
 	
+	protected void initialize(){
+	lengthOfTurn.start();
+	Robot.drivetrain.setSetpoint(rate);
+	Robot.drivetrain.startPID();
+	}
 	
+	protected void execute(){
+		
+	}
 	
 	
 	@Override
 	protected boolean isFinished() {
-		// TODO Auto-generated method stub
-		return false;
+		if(lengthOfTurn.get() >= seconds){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	protected void end(){
+		Robot.drivetrain.stopPID();
+		lengthOfTurn.stop();
+		lengthOfTurn.reset();
 	}
 }
