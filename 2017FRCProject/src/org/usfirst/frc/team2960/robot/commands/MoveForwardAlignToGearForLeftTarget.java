57ee280de8f4c8bee93281041177f0accb158642
malcolmmachesky <malcolm9292@gmail.com>
2017-03-03 17:48:42 -0500

    Change speed of auton turn

--- 2017FRCProject/src/org/usfirst/frc/team2960/robot/commands/MoveForwardAlignToGearForLeftTarget.java
+++ 2017FRCProject/src/org/usfirst/frc/team2960/robot/commands/MoveForwardAlignToGearForLeftTarget.java
@@ -5,7 +5,7 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class MoveForwardAlignToGearForLeftTarget extends CommandGroup{	
 	public MoveForwardAlignToGearForLeftTarget(){
 		addSequential(new MoveForward(-72));
-		addSequential(new TurnDegree(.75, 60));
+		addSequential(new TurnDegree(.75, 80));
 		addSequential(new AlignToGear());
 		addSequential(new Delay(20));
 		addSequential(new MoveForward(-30));
